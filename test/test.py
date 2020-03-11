a270888b91f33e4e1333a64820008aa40c9dc853
marinechaput <marine.chaput@nerf.be>
2020-03-11 14:17:13 +0100

    pre-commit clean

--- test/test.py
+++ test/test.py
@@ -1,36 +1,37 @@
 import unittest
 from unittest.mock import patch
+
 import dropbox
+import fsspec
+
 import dropboxdrivefs
 from dropboxdrivefs import DropboxDriveFileSystem
 
-import fsspec
 
-
-@patch.object(dropbox.Dropbox, 'files_list_folder')
+@patch.object(dropbox.Dropbox, "files_list_folder")
 class TestDropboxLs(unittest.TestCase):
-
     def setUp(self):
         self.fs = DropboxDriveFileSystem("123")
 
         self.folder_list = dropbox.files.ListFolderResult()
-        self.folder_list.entries = [dropbox.files.FileMetadata(name = "file1.txt", path_display = "file1.txt", size=20), dropbox.files.FileMetadata(name = "file2.txt", path_display = "file2.txt", size=30)]
-        self.folder_list.cursor = '123'
+        self.folder_list.entries = [
+            dropbox.files.FileMetadata(
+                name="file1.txt", path_display="file1.txt", size=20
+            ),
+            dropbox.files.FileMetadata(
+                name="file2.txt", path_display="file2.txt", size=30
+            ),
+        ]
+        self.folder_list.cursor = "123"
         self.folder_list.has_more = False
 
 
-
-
-
-
 class TestDropboxOpen(unittest.TestCase):
-
-
     def setUp(self):
         self.fs = DropboxDriveFileSystem("123")
 
-    @patch.object(dropbox.Dropbox, 'files_get_temporary_link')
-    @patch.object(dropboxdrivefs.DropboxDriveFileSystem, 'info')
+    @patch.object(dropbox.Dropbox, "files_get_temporary_link")
+    @patch.object(dropboxdrivefs.DropboxDriveFileSystem, "info")
     def test_open_rb(self, moke_info, moke_link):
 
         temp_link = dropbox.files.GetTemporaryLinkResult()
@@ -38,31 +39,27 @@ class TestDropboxOpen(unittest.TestCase):
         temp_link.link = "http://test_link"
         moke_link.return_value = temp_link
 
-        moke_info.return_value = {"size":20}
+        moke_info.return_value = {"size": 20}
 
-        reader = self.fs._open(
-                "/Home/folder1",
-                mode="rb"
-        )
+        reader = self.fs._open("/Home/folder1", mode="rb")
 
         self.assertIsInstance(reader, fsspec.implementations.http.HTTPFile)
 
     def test_open_wb(self):
-        with patch('dropboxdrivefs.core.DropboxDriveFile') as mock_subclass:
-            reader = self.fs._open(
-                "/Home/folder1",
-                mode="wb"
-            )
+        with patch("dropboxdrivefs.core.DropboxDriveFile") as mock_subclass:
+            reader = self.fs._open("/Home/folder1", mode="wb")
             self.assertIsInstance(reader, type(mock_subclass.return_value))
 
 
 class TestMetadata(unittest.TestCase):
-
     def setUp(self):
         self.fs = DropboxDriveFileSystem("123")
-        self.metadata = [dropbox.files.FileMetadata(name="file1.txt", path_display="file1.txt", size=20),
-                                    dropbox.files.FolderMetadata(name="files", path_display="files")]
-
+        self.metadata = [
+            dropbox.files.FileMetadata(
+                name="file1.txt", path_display="file1.txt", size=20
+            ),
+            dropbox.files.FolderMetadata(name="files", path_display="files"),
+        ]
 
     def test_info_file(self):
         info = self.fs._refactor_metadata(self.metadata[0])
@@ -79,16 +76,18 @@ class TestMetadata(unittest.TestCase):
         self.assertEqual(info["type"], "directory")
 
     def test_info(self):
-        info = self.fs._refactor_metadata(dropbox.files.Metadata(name = "file1.txt", path_display = "file1.txt"))
+        info = self.fs._refactor_metadata(
+            dropbox.files.Metadata(name="file1.txt", path_display="file1.txt")
+        )
         self.assertEqual(info["name"], "file1.txt")
         self.assertEqual(info["size"], None)
         self.assertEqual(info["type"], None)
 
     def test_info_no_details(self):
-        info = self.fs._refactor_metadata(self.metadata[0], detail = False)
+        info = self.fs._refactor_metadata(self.metadata[0], detail=False)
 
         self.assertEqual(info, "file1.txt")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
